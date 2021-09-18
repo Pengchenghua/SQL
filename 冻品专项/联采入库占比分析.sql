4f53c18495d0fb402cfcbd9c3cc437f26e2608bb
Pengchenghua <jopch@163.com>
2021-09-18 11:06:18 +0800

    1

--- 冻品专项/联采入库占比分析.sql
+++ 冻品专项/联采入库占比分析.sql
@@ -1,7 +1,7 @@
 set sdt='20210701';
 set edt='20210731';
 -- 来源订单：1 采购导入、10    智能补货、2 直送客户、14    临时地采
-drop table if exists csx_tmp.temp_entry_00 ;
+drop table if exists  csx_tmp.temp_entry_00 ;
 create table csx_tmp.temp_entry_00 as
 select  sdt,
     origin_order_no,
@@ -46,8 +46,8 @@ select sdt,
     0 shipped_amt,
     0 no_tax_shipped_amt
 from csx_dw.dws_wms_r_d_entry_batch a 
-where sdt>='20210801' 
-    and sdt<='20210831'
+where sdt>=${hiveconf:sdt} 
+    and sdt<=${hiveconf:edt} 
    -- and  regexp_replace( to_date(receive_time ),'-','')>='20210701'
     and order_type_code like 'P%'
    -- and business_type !='02'
@@ -70,8 +70,8 @@ from csx_dw.dws_wms_r_d_ship_detail  a
   where 1=1
     -- regexp_replace( to_date(send_time),'-','') >='20210801' 
 --     and  regexp_replace( to_date(send_time),'-','') <='20210831'
-    and sdt<'20210901'
-    and sdt>='20210801'
+    and sdt<=${hiveconf:edt} 
+    and sdt>=${hiveconf:sdt} 
     and order_type_code like 'P%'
     and business_type_code in ('05')
     and status in ('6','7','8')
@@ -138,6 +138,185 @@ group by origin_order_no,
     product_level_name
 ;
 
+
+
+-- 关联采购订单
+-- select * from csx_tmp.temp_entry_01 where sales_region_name ='大宗' and province_name='福建省'; 
+drop table if exists csx_tmp.temp_entry_01;
+create  table csx_tmp.temp_entry_01 as 
+select sdt,
+    sales_province_code,
+    sales_province_name,
+    sales_region_code,
+    sales_region_name,
+    city_code,
+    city_name,
+    province_code,
+    province_name,
+    purpose,
+    supplier_code,
+    vendor_name,
+    origin_order_no,
+    dc_code,
+    goods_code,
+    goods_name,
+    unit_name,
+    classify_large_code,
+    classify_large_name,
+    classify_middle_code,
+    classify_middle_name,
+    classify_small_code,
+    classify_small_name,
+    source_type,
+    source_type_name,
+    receive_qty,
+    receive_amt,
+    no_tax_receive_amt,
+    shipped_qty,
+    shipped_amt,
+    no_tax_shipped_amt,
+    joint_purchase,         --供应商联采标识 
+    joint_purchase_flag ,    --商品联采标识
+    product_level,
+    product_level_name,
+    price,
+    order_qty,
+    order_amt,
+    update_time,
+    create_by
+from 
+(select sdt,
+    sales_province_code,
+    sales_province_name,
+    sales_region_code,
+    sales_region_name,
+    a.dc_code,
+    company_code,
+    city_code,
+    city_name,
+    province_code,
+    province_name,
+    purpose,
+    origin_order_no,
+    a.order_code,
+    a.goods_code,
+    goods_name,
+    unit_name,
+    classify_large_code,
+    classify_large_name,
+    classify_middle_code,
+    classify_middle_name,
+    classify_small_code,
+    classify_small_name,
+    division_code,
+    division_name,
+    supplier_code,
+    vendor_name,
+    source_type,
+    source_type_name,
+     joint_purchase,         --供应商联采标识 
+    joint_purchase_flag ,    --商品联采标识
+    product_level,
+    product_level_name,
+    (receive_qty) receive_qty,
+    (receive_amt) receive_amt,
+    (no_tax_receive_amt) as no_tax_receive_amt,
+    (shipped_qty) shipped_qty,
+    (shipped_amt) shipped_amt,
+    (no_tax_shipped_amt) as no_tax_shipped_amt,
+    price,
+    order_qty,
+    order_amt,
+    update_time,
+    create_by
+from csx_tmp.temp_entry_00 a 
+join 
+(select  order_code,
+        source_type,
+        source_type_name,
+        header_remark,
+        items_remark,
+        goods_code,
+        price,
+        order_qty,
+        order_qty*price as order_amt,
+        update_time,
+        create_by
+    from csx_dw.dws_scm_r_d_header_item_price 
+    where super_class in ('2','1')  
+    and source_type in ('1','2','10','14')
+  
+)b on a.origin_order_no=b.order_code and a.goods_code=b.goods_code
+join 
+(select 
+    sales_province_code,
+    sales_province_name,
+    case when purchase_org ='P620' and purpose!='07' then '9' else  sales_region_code end sales_region_code,
+    case when purchase_org ='P620' and purpose!='07' then '大宗' else  sales_region_name end sales_region_name,
+    a.shop_id,
+    company_code,
+    case when a.shop_id in ('W0H4','W0G1','W0J8')  then '' else city_code end  city_code,
+    case when a.shop_id in ('W0H4','W0G1','W0J8')  then '' else city_name end  city_name,
+    case when a.shop_id in ('W0H4') then '900001' when a.shop_id in ('W0G1','W0J8')  then '900002' else province_code end province_code,
+    case when a.shop_id in ('W0H4') then '大宗二' when a.shop_id in ('W0G1','W0J8')  then '大宗一' else  province_name  end province_name,
+    purpose
+from csx_dw.dws_basic_w_a_csx_shop_m a 
+join 
+(select shop_id  from csx_ods.source_basic_frozen_dc where sdt=regexp_replace(date_sub(current_date(),1),'-','')  and is_frozen_dc='1') b on a.shop_id=b.shop_id
+ where sdt='current'    
+    and  table_type=1 
+    and purpose  in ('01','02','03','07','08','06') 
+) d on a.dc_code=d.shop_id
+) j 
+;
+
+set hive.exec.dynamic.partition.mode=nonstrict;
+insert overwrite table csx_tmp.ads_fr_r_d_frozen_purch_join_ratio partition(sdt)
+select
+    sales_province_code,
+    sales_province_name,
+    sales_region_code,
+    sales_region_name,
+    city_code,
+    city_name,
+    province_code,
+    province_name,
+    purpose,
+    supplier_code,
+    vendor_name,
+    origin_order_no,
+    dc_code,
+    goods_code,
+    goods_name,
+    unit_name,
+    classify_large_code,
+    classify_large_name,
+    classify_middle_code,
+    classify_middle_name,
+    classify_small_code,
+    classify_small_name,
+    source_type,
+    source_type_name,
+    receive_qty,
+    receive_amt,
+    no_tax_receive_amt,
+    shipped_qty,
+    shipped_amt,
+    no_tax_shipped_amt,
+    joint_purchase as joint_purchase_supplier,         --供应商联采标识 
+    joint_purchase_flag as joint_purchase_goods ,    --商品联采标识
+    product_level,
+    product_level_name,
+    price,
+    order_qty,
+    order_amt,
+    update_time as order_update_time,
+    create_by as order_create_by,
+    current_timestamp(),
+    sdt
+from 
+ csx_tmp.temp_entry_01 
+ ;
 
 
 -- 关联采购订单&DC类型&复用供应商
