3d53ceb048550492c13a2206966af30709a36335
Pengchenghua <jopch@163.com>
2024-05-14 16:53:06 +0800

    1

--- 财务需求/采购分析/-- 临时地采商品成本追溯销售跟踪.sql
+++ 财务需求/采购分析/-- 临时地采商品成本追溯销售跟踪.sql
@@ -7,7 +7,7 @@ select * from csx_analyse.csx_analyse_scm_purchase_order_flow_di where order_cod
 select * from   csx_dwd.csx_dwd_cas_accounting_stock_log_item_di  where sdt>='20230401' and link_wms_order_no='IN230627005052';
 
 -- 查找销售凭证单号
-select * from   csx_dwd.csx_dwd_cas_accounting_stock_log_item_di  where sdt>='20230401' and batch_no='CB20230627056574' and goods_code ='1279161';
+select * from   csx_dwd.csx_dwd_cas_accounting_stock_log_item_di  where sdt>='20230401' and batch_no='CB20240218145975' and goods_code ='1279161';
 
 -- 通过出库批次查找入库单号
 select * from   csx_dwd.csx_dwd_cas_accounting_stock_log_item_di  where sdt>='20230401' and link_wms_order_no='BG23062700004516'  and goods_code ='1279161';
